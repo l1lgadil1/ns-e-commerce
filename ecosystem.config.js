7e1241a2e1d14d64a82ec7711d5735b4905f446a
Ilyas Agadilov <agadilov.ilyas@mail.ru>
2024-08-30 00:12:11 +0500

    feat: add ecosystem deploy config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,17 @@ module.exports = {
       script: 'node_modules/next/dist/bin/next',
       args: '-p 3000',
       exec_mode: 'cluster',
-      instances: 'max'
-    }
-  ]
+    },
+  ],
+  deploy: {
+    production: {
+      user: process.env.REMOTE_SERVER_USERNAME,
+      host: process.env.REMOTE_SERVER_IP,
+      path: process.env.PROJECT_PATH,
+      repo: "git@github.com:T410/pm2-deploy",
+      ref: "origin/main",
+      key: process.env.SSH_KEY_PATH,
+      "post-deploy": "npm i; pm2 reload ecosystem.config.js --env production",
+    },
+  },
 };
