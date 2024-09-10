2a96027f7b8444e91ccc1c39005323e582477d22
Ilyas Agadilov <agadilov.ilyas@mail.ru>
2024-09-10 18:33:17 +0500

    fix: btn size

--- src/_pages/_product/ui/checkout-button.tsx
+++ src/_pages/_product/ui/checkout-button.tsx
@@ -8,14 +8,14 @@ interface IProps{
     isVisible:boolean;
 }
 export const CheckoutButton = ({ href, isVisible }:IProps) => (
-  <Flex className={cn('p-4 md:hidden fixed w-full bottom-0 left-0 transition-all ease-in', isVisible ? 'opacity-100' : 'opacity-0')}>
+  <Flex className={cn('p-4 md:hidden fixed w-full bottom-2 left-0 transition-all ease-in z-10', isVisible ? 'opacity-100' : 'opacity-0')}>
     <Link href={href} className='flex justify-center' target='_blank' rel='noreferrer'>
       <Button
         style={{
           background: `#8B253E`,
-          width: '100%'
+          width: '100%',
         }}
-        className='md:max-w-[600px] mx-auto '
+        className='md:max-w-[600px] mx-auto h-[44px] text-[18px]'
       >
         Оформить заказ
       </Button>
