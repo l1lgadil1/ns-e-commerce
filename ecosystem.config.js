dfd3d0d4484fd8ea98a91d201254b9bd58631f81
Ilyas Agadilov <agadilov.ilyas@mail.ru>
2024-09-10 19:01:23 +0500

    chore: change ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,22 +3,14 @@ module.exports = {
     {
       name: 'NS e-commerce',
       script: 'npm',
-      args: 'run start',
+      args: 'start',
       exec_mode: 'cluster',
       env: {
         PORT: 3000,
+        NODE_ENV: 'production', // Убедитесь, что NODE_ENV установлен в 'production'
       },
+      // Пост-запусковые команды
+      post_deploy: 'npm install && npm run build'
     },
   ],
-  deploy: {
-    production: {
-      user: "ubuntu",
-      host: "77.240.39.55",
-      ref: "origin/main",
-      repo: "git@github.com:T410/pm2-deploy",
-      path: "/home/ubuntu/ns-e-commerce", // Путь на сервере
-      key: "~/.ssh/id_rsa", // Путь до приватного ключа
-      "post-deploy": "npm i && npm run build && pm2 reload ecosystem.config.js --env production"
-    },
-  },
 };
